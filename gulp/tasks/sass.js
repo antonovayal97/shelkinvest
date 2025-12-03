75d886c02432a1d69173630fb2a2956834c830e6
Aial <antonovayal97@gmail.com>
2025-12-03 20:13:01 +0900

    fix

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,25 +1,57 @@
 module.exports = function () {
-    $.gulp.task('sass', function () {
-        return $.gulp.src('src/static/css/main.scss', { sourcemaps: true})
-            .on("error", $.gp.notify.onError({
-                message: "Error: <%= error.message %>",
-                title: "style"
-            }))
-            // .pipe($.gp.csso())
-            .pipe($.sass({ outputStyle: 'expanded' }))
-            .pipe($.gp.autoprefixer({
-                Browserslist: ["defaults"]
-            }))
-            // .pipe($.gp.sourcemaps.write('./'))
-            .pipe($.gulp.dest('build/css/'))
-            // Минифицированная версия
-            .pipe($.sass({ outputStyle: 'compressed' }))
-            .pipe($.gp.rename('main.min.css'))
-            .pipe($.gulp.dest('build/css/', { sourcemaps: './'}))
-            .on('end', $.bs.reload);
+  $.gulp.task("sass:main", function () {
+    return $.gulp
+      .src("src/static/css/main.scss", { sourcemaps: true })
+      .on(
+        "error",
+        $.gp.notify.onError({
+          message: "Error: <%= error.message %>",
+          title: "style",
+        })
+      )
+      .pipe($.sass({ outputStyle: "expanded" }))
+      .pipe(
+        $.gp.autoprefixer({
+          Browserslist: ["defaults"],
+        })
+      )
+      .pipe($.gulp.dest("build/css/"))
+      .pipe($.sass({ outputStyle: "compressed" }))
+      .pipe($.gp.rename("main.min.css"))
+      .pipe($.gulp.dest("build/css/", { sourcemaps: "./" }));
+  });
 
-        // .pipe($.bs.reload({
-        //     stream:true
-        // }));
-    });
-}
+  $.gulp.task("sass:blocks", function () {
+    return $.gulp
+      .src("src/static/css/blocks/*.scss", { sourcemaps: true })
+      .on(
+        "error",
+        $.gp.notify.onError({
+          message: "Error: <%= error.message %>",
+          title: "style",
+        })
+      )
+      .pipe($.sass({ outputStyle: "expanded" }))
+      .pipe(
+        $.gp.autoprefixer({
+          Browserslist: ["defaults"],
+        })
+      )
+      .pipe($.gulp.dest("build/css/blocks/"))
+      .pipe($.sass({ outputStyle: "compressed" }))
+      .pipe(
+        $.gp.rename(function (path) {
+          path.basename += ".min";
+        })
+      )
+      .pipe($.gulp.dest("build/css/blocks/", { sourcemaps: "./" }));
+  });
+
+  $.gulp.task(
+    "sass",
+    $.gulp.series($.gulp.parallel("sass:main", "sass:blocks"), function (done) {
+      $.bs.reload();
+      done();
+    })
+  );
+};
--- src/static/js/common.js
+++ src/static/js/common.js
@@ -18,77 +18,122 @@ window.addEventListener("load", () => {
   hidePageLoader();
 });
 
-function initMobileMenu() {
-  const burger = document.querySelector(".header__burger");
-  const mobileMenu = document.querySelector(".header__mobile-menu");
-  const mobileOverlay = document.querySelector(".header__mobile-overlay");
-  const closeButton = document.querySelector(".header__mobile-menu-close");
+function checkHeader() {
+  const header = document.querySelector(".header");
+
+  const mainPages = ["/", "/index.html"];
+
+  if (!mainPages.includes(location.pathname)) {
+    header.classList.add("header--pages");
+  }
+}
+function initDropdown() {
   const dropdownItems = document.querySelectorAll(
-    ".header__mobile-menu-item--dropdown"
+    ".c-mobile-menu-item--dropdown"
   );
 
-  if (!burger || !mobileMenu || !mobileOverlay) return;
-
-  function openMenu() {
-    mobileMenu.classList.add("header__mobile-menu--active");
-    mobileOverlay.classList.add("header__mobile-overlay--active");
-    document.body.style.overflow = "hidden";
-  }
-
-  function closeMenu() {
-    mobileMenu.classList.remove("header__mobile-menu--active");
-    mobileOverlay.classList.remove("header__mobile-overlay--active");
-    document.body.style.overflow = "";
-    dropdownItems.forEach((item) => {
-      item.classList.remove("active");
-      const submenu = item.querySelector(".header__mobile-submenu");
-      if (submenu) {
-        submenu.classList.remove("header__mobile-submenu--active");
-      }
-    });
-  }
-
-  burger.addEventListener("click", openMenu);
-  closeButton?.addEventListener("click", closeMenu);
-  mobileOverlay.addEventListener("click", closeMenu);
-
   dropdownItems.forEach((item) => {
-    const toggle = item.querySelector(".header__mobile-menu-link--toggle");
-    const submenu = item.querySelector(".header__mobile-submenu");
+    const toggle = item.querySelector(".c-mobile-menu-link--toggle");
+    const submenu = item.querySelector(".c-mobile-submenu");
 
     if (toggle && submenu) {
       toggle.addEventListener("click", () => {
         const isActive = item.classList.contains("active");
         if (isActive) {
           item.classList.remove("active");
-          submenu.classList.remove("header__mobile-submenu--active");
+          submenu.classList.remove("c-mobile-submenu--active");
         } else {
           dropdownItems.forEach((otherItem) => {
             otherItem.classList.remove("active");
-            const otherSubmenu = otherItem.querySelector(
-              ".header__mobile-submenu"
-            );
+            const otherSubmenu = otherItem.querySelector(".c-mobile-submenu");
             if (otherSubmenu) {
-              otherSubmenu.classList.remove("header__mobile-submenu--active");
+              otherSubmenu.classList.remove("c-mobile-submenu--active");
             }
           });
           item.classList.add("active");
-          submenu.classList.add("header__mobile-submenu--active");
+          submenu.classList.add("c-mobile-submenu--active");
         }
       });
     }
   });
+}
 
-  const mobileMenuLinks = mobileMenu.querySelectorAll(
-    ".header__mobile-menu-link:not(.header__mobile-menu-link--toggle)"
-  );
-  mobileMenuLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      setTimeout(closeMenu, 100);
-    });
-  });
+function initMobileMenu() {
+  const burger = document.querySelector(".header__burger");
+  const header = document.querySelector(".header");
+  const mobileMenu = document.querySelector(".header__mobile-menu");
+  const mobileOverlay = document.querySelector(".header__mobile-overlay");
+  const closeButton = document.querySelector(".header__mobile-menu-close");
+
+  let isOpen = false;
+
+  if (!burger || !mobileMenu || !mobileOverlay) return;
+
+  function openMenu() {
+    header.classList.add("header--mobile-menu-opened");
+    burger.classList.add("header__burger--active");
+    mobileMenu.classList.add("header__mobile-menu--active");
+    mobileOverlay.classList.add("header__mobile-overlay--active");
+    document.body.style.overflow = "hidden";
+    isOpen = true;
+  }
+
+  function closeMenu() {
+    header.classList.remove("header--mobile-menu-opened");
+    burger.classList.remove("header__burger--active");
+    mobileMenu.classList.remove("header__mobile-menu--active");
+    mobileOverlay.classList.remove("header__mobile-overlay--active");
+    document.body.style.overflow = "";
+    isOpen = false;
+  }
+
+  function toggleMenu() {
+    if (isOpen) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  }
+
+  burger.addEventListener("click", toggleMenu);
+  closeButton?.addEventListener("click", closeMenu);
+  mobileOverlay.addEventListener("click", closeMenu);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  initDropdown();
   initMobileMenu();
+  checkHeader();
+
+  const modal = new HystModal({
+    linkAttributeName: "data-hystmodal",
+    //settings (optional). see API
+  });
+
+  //modal.open("#modal-1");
+
+  let pageScroller = document.querySelector(".page-scroller");
+
+  pageScroller.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 1000) {
+      pageScroller.classList.add("page-scroller--active");
+    } else {
+      pageScroller.classList.remove("page-scroller--active");
+    }
+  });
+
+  let phones = document.querySelectorAll(".c-phone-input");
+
+  phones.forEach((phone) => {
+    const maskOptions = {
+      mask: "+{7} (000) 000-00-00",
+    };
+    const mask = IMask(phone, maskOptions);
+  });
 });
